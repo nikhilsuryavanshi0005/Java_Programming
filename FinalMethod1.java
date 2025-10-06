19f7bfe93fbe4af4ddf441849cf840f5eb5ded26
nikhilsuryavanshi0005 <nikhilsuryavanshi0005@gmail.com>
2025-10-06 23:28:18 +0530

    Add files via upload

---
+++ FinalMethod1.java
@@ -0,0 +1,34 @@
+class Base
+{
+    public void fun()     //Defination
+    {
+
+    }
+    public void gun()     //Defination
+    {
+
+    }
+
+}
+    
+class Derived extends Base
+{
+    public void run()   //defination
+    {
+
+    }
+    public void gun()   //overriding
+    {
+
+    }
+}
+
+class FinalData4
+{
+    public static void main(String Arr[])
+    {
+        Demo dobj = new Demo();
+        
+        
+    }
+}
---
+++ FinalMethod2.java
@@ -0,0 +1,34 @@
+class Base
+{
+    final public void fun()     //Defination
+    {
+
+    }
+    final public void gun()     //Defination
+    {
+
+    }
+
+}
+    
+class Derived extends Base
+{
+    public void run()   //defination
+    {
+
+    }
+    public void gun()   //overriding
+    {
+
+    }
+}
+
+class FinalMethod2
+{
+    public static void main(String Arr[])
+    {
+        
+        
+        
+    }
+}
